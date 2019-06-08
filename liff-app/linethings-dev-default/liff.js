eb535afaece2ad5b6090a0fcd144f821691cce85
【misato】 <uebo.misato@gmail.com>
2019-06-08 14:07:41 +0900

    LED5 checkbox update4

--- liff-app/linethings-dev-default/liff.js
+++ liff-app/linethings-dev-default/liff.js
@@ -380,7 +380,7 @@ async function updateLedState(device) {
     const led0_status = !!card.querySelector('.led0_value').checked;
     const led1_status = !!card.querySelector('.led1_value').checked;
     const led2_status = !!card.querySelector('.led2_value').checked;
-    const led3_status = !!card.querySelector('.led3_value').('.led3_id').checked;
+    const led3_status = !!card.querySelector('.led3_value').checked;
     const buzzer_status = !!card.querySelector('.buzzer_value').checked;
     const io2_status = !!card.querySelector('.io2_value').checked;
     const io3_status = !!card.querySelector('.io3_value').checked;
